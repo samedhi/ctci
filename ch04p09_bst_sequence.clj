f9e04b97419c0d1506f79ad180c1dbd35a561418
Stephen Cagle <stephen@read-line.com>
2020-05-26 13:19:10 -0700

    [BST Sequence] Problem 4.8 of Cracking the Coding Interview (6th Edition) Part 2
    Implemented the solution after reading the solution from the book. Biggest
    mistake was spending to much time collapsing the concat (whic I think can be
    solved by adding in a simple :post condition and failing to recognize that this
    is really two recursive functions.

--- ch04p09_bst_sequence.clj
+++ ch04p09_bst_sequence.clj
@@ -2,42 +2,49 @@
 
 (require '[clojure.test :as t])
 
-#_(defn bst-sequence-rec [root acc]
-    (let [[v l r] root
-          new-acc (conj acc v)]
-      (println (apply str (take (count acc) "  ")) :bst-sequence-rec :root root :acc acc :new-acc new-acc)
-      (apply
-       concat
-       (cond
-         (and (nil? l) (nil? r)) [[new-acc]]
-         (nil? l) [(bst-sequence-rec r new-acc)]
-         (nil? r) [(bst-sequence-rec l new-acc)]
-         :else [(bst-sequence-rec l (conj new-acc (first r)))
-                (bst-sequence-rec r (conj new-acc (first l)))]))))
+(defn weave
+  "weave xs and ys in every way while maintaining order of each"
+  ([xs ys]
+   (weave xs ys []))
+  ([xs ys zs]
+   (cond
+     (and (empty? xs) (empty? ys)) [zs]
+     (empty? xs) [(into zs ys)]
+     (empty? ys) [(into zs xs)]
+     :else
+     (let [[x & xrest] xs
+           [y & yrest] ys]
+       (concat
+        (weave xrest ys (conj zs x))
+        (weave xs yrest (conj zs y)))))))
 
-(defn bst-sequence-rec
-  "sequence of arrays that could have formed this node"
+(defn bst-sequence
+  "sequence of arrays that could have formed this BST node"
   [node]
+  {:post [(seq? %)
+          (every? (fn [xs] (every? number? xs)) %)]}
   (let [[v l r] node]
     (map
      #(cons v %)
      (cond
        (and (nil? l) (nil? r)) [nil]
-       (nil? l) (bst-sequence-rec r)
-       (nil? r) (bst-sequence-rec l)
+       (nil? l) (bst-sequence r)
+       (nil? r) (bst-sequence l)
        :else
        (apply
         concat
-        (for [l-seq (bst-sequence-rec l)
-              r-seq (bst-sequence-rec r)]
-          [(into (vec l-seq) r-seq)
-           (into (vec r-seq) l-seq)]))))))
-
-(defn bst-sequence [root]
-  (bst-sequence-rec root))
+        (for [l-seq (bst-sequence l)
+              r-seq (bst-sequence r)]
+          (weave l-seq r-seq)))))))
 
 (t/deftest test
-  (t/are [root expected] (= expected (-> root bst-sequence set))
+  (t/are [xs ys expected] (= expected (weave xs ys))
+    [] [] [[]]
+    [1] [] [[1]]
+    [] [1] [[1]]
+    [1] [2] [[1 2] [2 1]]
+    [1 2] [3 4] [[1 2 3 4] [1 3 2 4] [1 3 4 2] [3 1 2 4] [3 1 4 2] [3 4 1 2]])
+  (t/are [root expected] (= expected (->> root bst-sequence set))
     [2] #{[2]}
     [2 [1]] #{[2 1]}
     [2 [1] [3]] #{[2 1 3] [2 3 1]}
@@ -45,8 +52,6 @@
     [1 nil [2 nil [3]]] #{[1 2 3]}
     [3 [2 [1]]] #{[3 2 1]}
     [3 [2 [1]] [4]] #{[3 4 2 1] [3 2 1 4] [3 2 4 1]}
-    ;; This isn't right, what about [3 4 2 1 5] for instance?
-    [3 [2 [1]] [4 nil [5]]] #{[3 4 5 2 1] [3 2 1 4 5] [3 4 2 1 5] [3 2 4 1 5]}
-    ))
+    [3 [2 [1]] [4 nil [5]]] #{[3 4 5 2 1] [3 2 1 4 5] [3 4 2 1 5] [3 2 4 1 5] [3 2 4 5 1] [3 4 2 5 1]}))
 
 (t/run-tests *ns*)
